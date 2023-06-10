65fc25856f87ec7f5a003b8eda32c19f54bb7b70
Moran-0 <2857599224@qq.com>
2023-06-10 09:05:41 +0800

    Vue基础

---
+++ JavaWebLearning/ajax-demo/src/main/java/com/moran/json/FastJsonDemo.java
@@ -0,0 +1,29 @@
+package com.moran.json;
+
+import com.alibaba.fastjson.JSON;
+
+/**
+ * @moran
+ * 测试下json和Java字符串的相互转换
+ */
+public class FastJsonDemo {
+
+    public static void main(String[] args) {
+        //1. 将Java对象转为JSON字符串
+        User user = new User();
+        user.setId(1);
+        user.setUsername("zhangsan");
+        user.setPassword("123");
+
+        String jsonString = JSON.toJSONString(user);
+        System.out.println(jsonString);//{"id":1,"password":"123","username":"zhangsan"}
+
+
+        //2. 将JSON字符串转为Java对象
+
+        User u = JSON.parseObject("{\"id\":1,\"password\":\"123\",\"username\":\"zhangsan\"}", User.class);
+        System.out.println(u);
+
+
+    }
+}
