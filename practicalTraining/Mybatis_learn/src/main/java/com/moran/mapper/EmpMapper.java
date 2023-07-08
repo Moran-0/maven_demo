d2b4232696f3dcc26a935a77f35d0dc3d56452e3
Moran-0 <2857599224@qq.com>
2023-07-08 12:51:50 +0800

    基本完成

---
+++ practicalTraining/Mybatis_learn/src/main/java/com/moran/mapper/EmpMapper.java
@@ -0,0 +1,5 @@
+package com.moran.mapper;
+
+public interface EmpMapper {
+
+}
---
+++ practicalTraining/Mybatis_learn/src/main/java/com/moran/pojo/Emp.java
@@ -0,0 +1,14 @@
+package com.moran.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Emp {
+    private String deptNo;
+    private String depName;
+    private String loc;
+}
